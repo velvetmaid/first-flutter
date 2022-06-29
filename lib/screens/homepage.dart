f6472a65c56ef86b2d30480cb3da5e700ed1f890
Camieux <tarsisiusadhi@gmail.com>
2022-06-29 15:51:55 +0700

    setup-lecasacrud

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -9,14 +9,35 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
+          backgroundColor: Color.fromARGB(255, 178, 255, 71),
           title: Text('Le Casa CRUD'),
         ),
-        body: Container(
-          child: Center(
-              child: Text(
-            'Halo',
-            style: TextStyle(fontSize: 20.0),
-          )),
-        ));
+        body: Stack(children: [
+          ListView(
+            children: [
+              //// VIEW DATA HERE
+              SizedBox(
+                height: 150,
+              )
+            ],
+          ),
+          Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 15),
+                  decoration:
+                      BoxDecoration(color: Colors.amberAccent, boxShadow: [
+                    BoxShadow(
+                        color: Colors.black12,
+                        offset: Offset(-5, 0),
+                        blurRadius: 15,
+                        spreadRadius: 3)
+                  ]),
+                  width: double.infinity,
+                  height: 130,
+                  child: Row(
+                    children: [],
+                  )))
+        ]));
   }
 }
